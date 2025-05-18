d0dd0edc19ae5cb40c1ca6681919abadd6c1969a
FooHuiHui <49934195+FooHuiHui@users.noreply.github.com>
2025-05-18 09:09:10 +0000

    Simple add food and display

---
+++ script.js
@@ -0,0 +1,12 @@
+const API_URL = "http://localhost:5000";
+let token = "";
+
+async function addFood() {
+    const food_name = document.getElementById("food_name").value;
+    const quantity = document.getElementById("quantity").value;
+    const expiry_date = document.getElementById("expiry_date").value;
+    const reminder_days = document.getElementById("reminder_days").value;
+
+    console.log(food_name)
+
+}
