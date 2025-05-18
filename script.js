ab0951aa520bb4f146c89588e9a4d664eacc838c
FooHuiHui <49934195+FooHuiHui@users.noreply.github.com>
2025-05-18 09:36:04 +0000

    removed everything

--- script.js
+++
@@ -1,9 +0,0 @@
-async function addFood() {
-    const food_name = document.getElementById("food_name").value;
-    const quantity = document.getElementById("quantity").value;
-    const expiry_date = document.getElementById("expiry_date").value;
-    const reminder_days = document.getElementById("reminder_days").value;
-
-
-}
-
