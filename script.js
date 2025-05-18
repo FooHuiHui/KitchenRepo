43525a37df8962e1c6455c84a395b6df2db35d74
FooHuiHui <49934195+FooHuiHui@users.noreply.github.com>
2025-05-18 09:51:01 +0000

    change const to var

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 async function addFood() {
-    const food_name = document.getElementById("food_name").value;
+    var food_name = document.getElementById("food_name").value;
     const quantity = document.getElementById("quantity").value;
     const expiry_date = document.getElementById("expiry_date").value;
     const reminder_days = document.getElementById("reminder_days").value;
+
 
 }
