bef7a3cad0ffc0094dd1e8d7517584f499488144
FooHuiHui <49934195+FooHuiHui@users.noreply.github.com>
2025-05-18 09:47:46 +0000

    try quantity

---
+++ script.js
@@ -0,0 +1,7 @@
+async function addFood() {
+    const food_name = document.getElementById("food_name").value;
+    const quantity = document.getElementById("quantity").value;
+    const expiry_date = document.getElementById("expiry_date").value;
+    const reminder_days = document.getElementById("reminder_days").value;
+
+}
